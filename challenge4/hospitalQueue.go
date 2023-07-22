8dca16c9a9cf55093e714484f1e4ff848b4e1770
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-07-22 00:39:01 -0300

    Desafio 4: Printando a lista ligada na tela

--- challenge4/hospitalQueue.go
+++ challenge4/hospitalQueue.go
@@ -2,15 +2,104 @@ package hospitalQueue
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
 )
 
-func GinServer() {
+type Patient struct {
+	Name string
+	Age  uint8
+}
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+type QueuePosition struct {
+	data Patient
+	next *QueuePosition
+}
+
+type HospitalQueue struct {
+	first *QueuePosition
+	last  *QueuePosition
+	size  uint16
+}
+
+func (hq *HospitalQueue) add(patient Patient) {
+	if hq.first == nil || hq.last == nil {
+		hq.first = &QueuePosition{
+			data: patient,
+			next: nil,
+		}
+		hq.last = hq.first
+		hq.size = 1
+	} else {
+		hq.last.next = &QueuePosition{
+			data: patient,
+			next: nil,
+		}
+
+		hq.last = hq.last.next
+		hq.size = hq.size + 1
+	}
+}
+
+func (hq *HospitalQueue) unroll() []Patient {
+	next := hq.first
+	unwoundQueue := make([]Patient, hq.size)
+
+	for i, _ := range unwoundQueue {
+		if next != nil {
+			unwoundQueue[i] = next.data
+		} else {
+			lastValidIndex := i - 1
+
+			hq.size = uint16(lastValidIndex + 1)
+			unwoundQueue = unwoundQueue[0:(lastValidIndex + 1)]
+			break
+		}
+
+		next = next.next
+	}
+
+	return unwoundQueue
+}
+
+type QueuePageData struct {
+	Data []Patient
+}
+
+func GinServer() {
+	port := os.Getenv("PORT")
+	htmlPath := os.Getenv("HTML_PATH")
+
+	if port != "" && htmlPath != "" {
+		sourceQueue := HospitalQueue{
+			first: nil,
+			last:  nil,
+			size:  0,
+		}
+		sourceQueue.add(Patient{
+			Name: "Lucas",
+			Age:  37,
 		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+		sourceQueue.add(Patient{
+			Name: "Gabriel",
+			Age:  73,
+		})
+
+		testData := QueuePageData{
+			Data: sourceQueue.unroll(),
+		}
+
+		router := gin.Default()
+		router.LoadHTMLGlob(htmlPath)
+
+		router.GET("/test", func(context *gin.Context) {
+			context.HTML(http.StatusOK, "index.html", testData)
+		})
+
+		router.Run(":" + port)
+	} else {
+		log.Fatal("No port defined!")
+	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	hospitalQueue "LearnGo/challenge4"
+	"os"
 )
 
 func main() {
+	os.Setenv("PORT", "8080")
+	os.Setenv("HTML_PATH", "challenge4/**/*.html")
 	hospitalQueue.GinServer()
 }
