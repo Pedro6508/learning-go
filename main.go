2c8d12882900366d732bbc0c3545a4653c7c326b
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-07-25 10:27:32 -0300

    novo formato de desafio

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	hospitalQueue "LearnGo/challenge4"
-	"log"
-	"os"
-)
-
-const (
-	MAX_PROCS_NUM = 5
-)
+import movieWar "LearnGo/challenge5"
 
 func main() {
-	errA := os.Setenv("PORT", "8080")
-	errB := os.Setenv("HTML_PATH", "challenge4/**/*.html")
-
-	if errA == nil && errB == nil {
-		hospitalQueue.GinServer()
-	} else {
-		if errA != nil {
-			log.Fatal(errA)
-		}
-
-		log.Fatal(errB)
-	}
+	movieWar.EnviarTarefas()
 }
